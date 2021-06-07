63f5e291691c23cab7e002d12ee724627a8a99f4
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-07 14:46:18 -0300

    Predicate

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,10 +12,13 @@ public class Program {
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.0));
-		list.add(new Product("Notebook", 1200.0));
-		list.add(new Product("Tablet", 450.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("HD Case", 80.90));
+		list.add(new Product("Tablet", 350.0));
 		
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		double min = 100.0;
+				
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);
